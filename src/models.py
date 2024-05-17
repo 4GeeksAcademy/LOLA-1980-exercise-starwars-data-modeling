28390c1b843037ed44cc46db40dc59fa3047073a
LOLA-1980 <109870340+LOLA-1980@users.noreply.github.com>
2024-05-17 16:40:26 +0000

    [Modelo de StarWars Terminado]

--- src/models.py
+++ src/models.py
@@ -7,6 +7,59 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__= 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    email = Column(String(50), unique=True)
+    password = Column(String(32))
+
+class Planets(Base):
+    __tablename__= 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    population = Column(Integer)
+
+class FavoritePlanets(Base):
+    __tablename__= 'favorite_planets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id_relationship = relationship(User)
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    planets_id_relationship = relationship(Planets)
+
+class Characters(Base):
+    __tablename__= 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    height = Column(Integer)
+    mass = Column(Integer)
+
+class FavoriteCharacters(Base):
+    __tablename__= 'favorite_characters'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id_relationship = relationship(User)
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    characters_id_relationship = relationship(Characters)
+
+class Starships(Base):
+    __tablename__='starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    model = Column(Integer)
+    lenght = Column(Integer)
+
+class FavoriteStarships(Base):
+    __tablename__= 'favorite_starships'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id_relationship = relationship(User)
+    starships_id = Column(Integer, ForeignKey('starships.id'))
+    starships_id_relationship = relationship(Starships)
+
+
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
